ff38c1878130cf40bc5cf5830dd34e59ff720c83
Elvis Rugamba <elvisrugamba@gmail.com>
2025-01-17 15:00:03 +0200

    [fix]: Fix navigatrion bar responsiveness

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -1,29 +1,51 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@heroui/navbar";
+'use client'
+
+import { useState } from "react";
 import Link from "next/link";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem} from "@heroui/navbar";
 
 export default function NavigationBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = [{
+    label: 'Home',
+    href: '/'
+  },{
+    label: 'About',
+    href: '/about'
+  },{
+    label: 'Contact',
+    href: '/contact'
+  }
+]
+
   return (
-    <Navbar isBordered={true}>
+    <Navbar isBordered={true} isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+      <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
       <NavbarBrand>
         <p className="font-bold text-inherit">CC Manager</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link href="/">
-            Home
+        {menuItems?.map((menuItem, index) => (
+          <NavbarItem key={`${menuItem.label}-${index}`}>
+          <Link href={menuItem.href}>
+            {menuItem.label}
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link href="/about">
-            About
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="/contact">
-            Contact
-          </Link>
-        </NavbarItem>
+        ))}
       </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((menuItem, index) => (
+          <NavbarMenuItem key={`${menuItem}-${index}`}>
+            <Link href={menuItem.href}>
+            {menuItem.label}
+          </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   )
 }
